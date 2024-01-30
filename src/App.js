81cce65fb1fae6e3795872fda58dfb305c5231f5
honghanh <hanhnguyen016012@gmail.com>
2024-01-30 13:28:10 +0700

    upload UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import OrderStaff from './pages/OrderStaff'
-
+import HomeStaff from './pages/HomeStaff';
+import BookingStaff from './pages/BookingStaff';
+import IntroLocation from './pages/IntroLocation';
+import BookingNow from './pages/BookingNow';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
     return ( 
        <BrowserRouter>
@@ -11,6 +15,10 @@ function App() {
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/orderstaff' element={<OrderStaff/>}/>
+        <Route path='/homestaff' element={<HomeStaff/>}/>
+        <Route path='/bookingstaff' element={<BookingStaff/>}/>
+        <Route path='/introlocation' element={<IntroLocation/>}/>
+        <Route path='/bookingnow' element={<BookingNow/>}/>
        </Routes>
        </BrowserRouter>
     );
---
+++ src/pages/BookingNow/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+function BookingNow() {
+  return (
+    <div class="desktop-8">
+      <div class="t-b-n">Đặt bàn</div>
+      <div class="ch-n-a-i-m">Chọn địa điểm</div>
+      <div class="rectangle-108"></div>
+      <div class="rectangle-113"></div>
+      <div class="rectangle-114"></div>
+      <div class="rectangle-112"></div>
+      <div class="chi-nh-nh-1">Chi nhánh 1</div>
+      <div class="chi-nh-nh-3">Chi nhánh 3</div>
+      <div class="chi-nh-nh-4">Chi nhánh 4</div>
+      <div class="chi-nh-nh-2">Chi nhánh 2</div>
+      <img class="rectangle-115" src="../asset/cafethucung.png" />
+      <div class="rectangle-116"></div>
+      <div class="tr-c-khi-b-n-t">TRƯỚC KHI BẠN ĐẶT</div>
+      <div class="vi-c-t-ch-c-a-b-n-s-ch-c-x-c-nh-n-khi-b-n-nh-n-c-email-x-c-nh-n">
+        Việc đặt chỗ của bạn sẽ chỉ được xác nhận khi bạn nhận được email xác
+        nhận.
+      </div>
+      <div class="m-b-o-s-c-kh-e-v-th-tr-ng-t-t-cho-m-o-c-a-ch-ng-t-i-c-duy-tr-ti-u-chu-n-cao-nh-t-ch-ng-t-i-ph-i-tr-ph-ph-c-l-i-7-cho-m-i-ng-i-l-n-v-5-cho-m-i-tr-em-th-i-gian-t-ch-l-m-t-gi-nh-ng-n-u-b-n-mu-n-t-ch-l-u-h-n-vui-l-ng-li-n-h-v-i-ch-ng-t-i">
+        Để đảm bảo sức khỏe và thể trạng tốt cho mèo của chúng tôi được duy trì
+        ở tiêu chuẩn cao nhất, chúng tôi phải trả phí phúc lợi £7 cho mỗi người
+        lớn và £5 cho mỗi trẻ em. Thời gian đặt chỗ là một giờ, nhưng nếu bạn
+        muốn đặt chỗ lâu hơn, vui lòng liên hệ với chúng tôi.
+      </div>
+    </div>
+  );
+}
+
+export default BookingNow;
