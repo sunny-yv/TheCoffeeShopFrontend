5d3685e3951869225b8359c83d75629b26d883de
nguyenhanh1612 <144312854+nguyenhanh1612@users.noreply.github.com>
2024-02-22 13:40:45 +0700

    edit homepage

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -68,7 +68,7 @@ function index() {
           giãn tối đa hay chỉ là một trải nghiệm độc đáo và thú vị thì Kitty
           Café là nơi dành cho bạn!
         </p>
-       
+
         <br />
         <br />
         <p>Kitty Cafe Nottingham hiện chỉ có THẺ.</p>
@@ -82,22 +82,38 @@ function index() {
         <br />
         <br />
         <p> Số giấy phép - 136453</p>
-        <button>Đặt bàn</button>
+        <Button
+          variant="outlined"
+          href="#outlined-buttons"
+          style={{
+            backgroundColor: "#9e826c",
+            color: "white",
+            borderColor: "#9e826c",
+            marginLeft: "600px"
+          }}
+        >
+          <Link
+            to="/bookingnow"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            Đặt bàn
+          </Link>
+        </Button>
       </div>
       <div class="text-location-right">
-        <div className="address">
+        <div className="column">
           <h4>Địa chỉ</h4>
           <p> 119 Nguyễn Thị Minh Khai, P. Bến Thành, Q.1</p>
         </div>
-        <div className="time">
+        <div className="column">
           <h4>Giờ mở cửa</h4>
           <p>Thứ 2 : 2:00pm to 9:30pm</p>
           <p>Thứ 3-Thứ 7: 1:00pm to 9:30pm</p>
           <p>Chủ nhật: 9:30 am to 10:00pm</p>
         </div>
-        <div className="contact">
+        <div className="column">
           <h4>Liên hệ</h4>
-          <p>Phone: 0961898942</p>
+          <p>Phone: 0961898942</p>
           <p>Email: CatfeVN@gmail.com</p>
         </div>
       </div>
